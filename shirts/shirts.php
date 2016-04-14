71e9c19fde5a8ff3ec2bc4309e2ad5a61ff83b21
Sagar M <sagardoesweb@gmail.com>
2016-04-14 11:24:32 +0530

    paginate list controller

--- shirts/shirts.php
+++ shirts/shirts.php
@@ -2,9 +2,36 @@
 
 	require_once("../inc/config.php");
 	require_once(ROOT_PATH . "inc/products.php");
-	$products = get_products_all();
 
-?><?php 
+	if (empty($_GET['pg'])) {
+		$current_page = 1;
+	} else {
+		$current_page = $_GET['pg'];
+	}
+
+	$current_page = intval($current_page);
+
+	$total_products =get_products_count();
+	$products_per_page = 8;
+	$total_pages = ceil($total_products / $products_per_page);
+
+	if ($current_page > $total_pages) {
+		header("Location: ./pg?=" . $total_pages);
+	}
+
+	if ($current_page < 1) {
+		header("Location: ./");
+	}
+
+	$start = (($current_page - 1)* $products_per_page) + 1;
+	$end = $current_page * $products_per_page;
+	if ($end > $total_products) {
+		$end = $total_products;
+	}
+
+	$products = get_products_subset($start,$end);
+
+?><?php
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include(ROOT_PATH . 'inc/header.php'); ?>
@@ -16,7 +43,7 @@ include(ROOT_PATH . 'inc/header.php'); ?>
 				<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
 
 				<ul class="products">
-					<?php foreach($products as $product) { 
+					<?php foreach($products as $product) {
 							echo get_list_view_html($product);
 						}
 					?>
