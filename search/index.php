c49ca7eaa42957b53a16c1de5a4bc254ec01ede1
Sagar M <sagardoesweb@gmail.com>
2016-04-14 10:13:35 +0530

    Add search model

--- search/index.php
+++ search/index.php
@@ -1,46 +1,49 @@
 <?php
 
-require_once( "../inc/config.php" );
+require_once("../inc/config.php");
 
-$search_term = '';
-if (isset($_GET['s'])) {
-    $search_term = trim($_GET['s']);
-    if ($search_term != '') {
-        require_once(ROOT_PATH.'inc/products.php');
-        $products = get_products_search($search_term);
-    }
+$search_term = "";
+if (isset($_GET["s"])) {
+	$search_term = trim($_GET["s"]);
+	if ($search_term != "") {
+		require_once(ROOT_PATH . "inc/products.php");
+		$products = get_products_search($search_term);
+	}
 }
 
-$pageTitle = 'Search';
-$section = 'search';
-include(ROOT_PATH . 'inc/header.php'); ?>
+$pageTitle = "Search";
+$section = "search";
+include(ROOT_PATH . "inc/header.php"); ?>
 
-<div class="section shirts search page">
-    <div class="wrapper">
-    <h1>Search</h1>
-    <form method="get" action="./">
-        <input type="text" name="s" value="<?php echo htmlspecialchars($search_term); ?>">
-        <input type="submit" value="Go">
-    </form>
+	<div class="section shirts search page">
 
-    <?php
+		<div class="wrapper">
 
-    if ($search_term != "") {
-        if (!empty($products)) {
-            echo "<ul class="products">";
-            foreach ($products as $product) {
-                echo get_list_view_html($product);
-            }
-            echo "</ul>";
-        } else {
-            echo "<p>No products were found matching that search term.</p>";
-        }
-    }
+			<h1>Search</h1>
 
-    ?>
+			<form method="get" action="./">
+				<input type="text" name="s" value="<?php echo htmlspecialchars($search_term); ?>">
+				<input type="submit" value="Go">
+			</form>
 
-    </div>
-</div>
+			<?php
 
+				if ($search_term != "") {
+					if (!empty($products)) {
+						echo '<ul class="products">';
+						foreach ($products as $product) {
+							echo get_list_view_html($product);
+						}
+						echo '</ul>';
+					} else {
+						echo '<p>No products were found matching that search term.</p>';
+					}
+				}
 
-<?php include(ROOT_PATH.'inc/footer.php');
+			?>
+
+		</div>
+
+	</div>
+
+<?php include(ROOT_PATH . "inc/footer.php"); ?>
